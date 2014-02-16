21b8b50d570a8412a2b01b31c4a558823d206c0c
Rich Smith <rich@syndis.is>
2014-02-16 12:41:57 -0500

    Initial commmit

---
+++ README.md
@@ -0,0 +1,67 @@
+#VMWare Guest URL Opener
+
+This is URL handler that is able to delegate and open URL's in a VMWare Fusion guest OS on OS X. It should work against
+most guest OS's and open the link in whatever is set as the default browser for the guest.
+
+There is also an option to provide a list of regular expressions to open in the host OS rather than the guest.
+
+
+##Assisted Installation and Setup
+
+There is a simple installer that should work for a majority of use cases, run it by changing to `vmware_guest_url_opener` directory the simply doing:
+
+```
+python install.py
+```
+
+You will then run the setup routines to generate the application bundle and be prompted for a variety of things required for setup and the resulting config file will be written to `~\.vmware_guest_url_opener.cfg`
+
+##Manual Installation and Setup
+
+If you want to install and configured manually do the following, in the `vmware_guest_url_opener` directory run the following to build the app:
+
+```
+python setup.py py2app
+```
+
+This will build an OS X application bundle and dump it in the `dist` subdirectory. Place the resulting binary wherever you would like.
+
+
+Next you need to setup the configuration file use the example as a guide, it is quite straight forward and needs the following variables:
+
+* `vmx_path` - This is the path to the `.vmx` file of the VM guest in which you want links to open
+* `user` - This is the username of a user running in the guest VM in which links will open
+* `password` - This is the password of the user, not too desirable but VMWare gives us little choice in this
+* `bin` - This is the path to the `vmrun` binary, if you have VMWare installed in `/Applications` you can leave it at the default value in the example config
+
+The next two variables determine the behaviour of the URL handler:
+
+* `host_urls` - A list of regular expressions separated by `;` against which URL's will be tested, if the URL's match then the URL will not be opened in the guest but in the host
+* `host_browser` - The is the browser that any URL matching the `host_url` list will be opened in on the host. Supported values are: `chrome`, `safari` and `firefox`
+
+Once you have setup the config file you need to copy it to `~/.vmware_guest_url_opener.cfg` - **remember to set appropriate permissions on it as it has a password in it**
+
+The following variables are set by the `install.py` script so as to be able to restore the URL handlers that were previously set:
+
+* `http_handler` - The identifier of the existing default http handler e.g. com.google.chrome
+* `https_handler` - The identifier of the existing default https handler e.g. com.google.chrome
+
+The final step is to set this application as the default URL handler for applications so that it intercepts future URLs,
+to do this just do:
+
+```
+python vmware_guest_url_opener.py register
+```
+
+##Uninstall
+
+An uninstaller script is provided, it will reset the URL handlers to whatever they were before VMWareGuestURLOpener was installed.
+To uninstall simply do:
+
+```
+python uninstall.py
+```
+
+##References
+
+The Apple Event code for handling the URL events was on the technique used by [https://github.com/irq0/osx-url-open-handler]()
---
+++ install..py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------
+# Filename      : install..py.py
+# Description   : VMWareGuestURLOpener quick installation script
+# Created By    : Rich Smith
+# Date Created  : 15-Feb-2014 08:36
+# 
+# License       : BSD
+#-----------------------------------------------------------
+__author__ = "Rich Smith"
+__version__ = "0.1"
+
+import os
+import sys
+import getpass
+import ConfigParser
+import distutils.core
+from vmware_guest_url_opener import RegisterHandler, GetCurrentDefaultHandlers
+
+class Install(object):
+    """
+    Quicky installer to make setup easier
+    """
+    def __init__(self, config_file = "~/.vmware_guest_url_opener.cfg"):
+        """
+        """
+        self.config      = ConfigParser.RawConfigParser()
+        self.config_dest = os.path.expanduser(config_file)
+
+        self.browser_choices = ["chrome", "firefox", "safari"]
+        self.fusion_app      = "/Applications/VMware Fusion.app"
+        self.vmrun_bin       = "Contents/Library/vmrun"
+
+
+    def __call__(self):
+        """
+        Do installation / setup actions
+        """
+        print "\n** VMWare Guest URL Opener installation and setup **\n"
+
+        #Check we are on OSX
+        if sys.platform != 'darwin':
+            print "[!] Sorry VMWareGuestURLOpener only runs on OS X with VMWare Fusion at the moment :-( "
+            return False
+
+        #Run the setup
+        raw_input("Press enter to build the VMWareGuestURLOpener application package for your system (Lots of text will scroll by!) .....")
+        dist = distutils.core.run_setup('setup.py', ['-q', 'py2app'], stop_after = "run")
+
+        #Get config values
+        raw_input("\n\nDone. Now we are going to get some information from you to setup your VMWareGuestURLOpener environment. hit Enter to continue  ")
+        self.get_and_set_config()
+
+        #Copy config to ~ & chmod it to provide the pw a modicum of protection
+        with open(self.config_dest, 'wb') as configfile:
+            self.config.write(configfile)
+        os.chmod(self.config_dest, 0600)
+
+        #Register handler
+        print "\nRegistering VMWareGuestURLOpener as the default URL handler...",
+        RegisterHandler()
+        print "done."
+
+        print "\n\nInstallation Complete"
+        print "\n** The VMWareGuestURLOpener .app can be found in the 'dist' subdirectory, please move this to wherever you would like it to be permanently **\n"
+
+
+        return True
+
+
+    def _get_user_input(self, msg, default = None, required = False, non_echo = False, choices = []):
+        """
+        Query user for input and check value supplied
+        """
+        if non_echo:
+            ret = getpass.getpass(msg)
+        else:
+            ret = raw_input(msg)
+
+        if not ret and default:
+            ret = default
+
+        elif not ret and required:
+            print "[!] Please enter a non-empty value"
+            return self._get_user_input(msg, default, required, non_echo, choices)
+
+        elif choices and ret not in choices:
+            print "[!] Please enter a choice from one of the following: %s"%(choices)
+            return self._get_user_input(msg, default, required, non_echo, choices)
+
+        return ret
+
+
+    def get_and_set_config(self):
+        """
+        Query user for variables and create config file
+        """
+        ##Get values from the user
+        vmx_path = self._get_user_input("\nWhat is the path to the .vmx file of the guest VM you want links to open in? ", required = True)
+        vm_user  = self._get_user_input("What is the username of the guest VM? ", required = True)
+        vm_pass  = self._get_user_input("What is the password of the guest VM user? ", non_echo = True)
+
+        host_urls    = self._get_user_input("You can specify links to open in the host rather than the guest VM.\n\
+These should be specified as a list of regular expressions to match against destination URLs seperated by ';'.\n\
+(e.g. '[*.]google.com;[*.]github.com' (without quotes). Leave blank if you want all URL's to be opened in the guest: ")
+        host_browser = self._get_user_input("What browser would you like to open any host links in? %s "%(self.browser_choices), required = True, choices = self.browser_choices)
+
+        fusion_bin   = self._get_user_input("Where is your VMWare Fusion application installed? [%s] "%(self.fusion_app), default = self.fusion_app )
+
+
+        ##Record the current http/https handler so they can be easily restored later if needed
+        http_handler, https_handler = GetCurrentDefaultHandlers()
+
+        ##Build config
+        self.config.add_section("config")
+        self.config.set("config", "user", vm_user)
+        self.config.set("config", "password", vm_pass)
+        self.config.set("config", "vmx_path", os.path.expanduser(vmx_path))
+        self.config.set("config", "host_urls", host_urls)
+        self.config.set("config", "host_browser", host_browser)
+        self.config.set("config", "bin", os.path.join(os.path.expanduser(fusion_bin), self.vmrun_bin))
+        self.config.set("config", "orig_http_handler", http_handler)
+        self.config.set("config", "orig_https_handler", https_handler)
+
+
+if __name__ == "__main__":
+    """
+    Call installation
+    """
+    I = Install()
+    ret = I()
